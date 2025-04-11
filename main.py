89a560cd3dba5810108e955d26b4c0d48a95dbb5
D-JPM <DeclanJPMetcalfe@protonmail.com>
2025-04-11 01:56:23 +0100

    Added multi-target search with debug statements and fixed variable name bug

--- main.py
+++ main.py
@@ -12,24 +12,39 @@ def main():
     print(f"[DEBUG] Successfully loaded data with shape: {df.shape}") # Debugging
 
     # Defining patterns I want to count
-    target = 'Nottingham'
-    count = 0 # Counter for the target
-    
-    print(f"[DEBUG] Starting search or target word: {target}") # Debugging
+    targets = ['Nottingham', 'st davids', 'cambridge', 
+    'leicester', 'inverness', 'milton keynes', 'southamptop',
+    'carlise', 'chester', 'wells', 'coventry', 'belfast',
+    'doncaster', 'preston', 'dundee', 'swansee', 'dunfermline',
+    'york', 'edinburgh', 'newry', 'oxford', 'peterborough', 'plymouth',
+    'portsmouth', 'southend-on-sea', 'ripon', 'st albans', 'armagh',
+    'bangor', 'st asaph', 'lichfield', 'sunderland', 'lincoln', 'wakefield',
+    'lisburn', 'wrexham', 'londonderry', 'aberdeen', 'manchester']
 
-    # Loop through each column name in the df 
-    for column in df.columns: 
-        print(f"[DEBUG] Inspecting column: {column}") # Debugging
-        # For each column, loop through every cell
-        for cell in df[column]:
-            # Make sure the cell is a string before searching
-            # Check 1. is the cell a strin (isinstance)
-            # Check 2. Does the target word appear in the cell
-            if isinstance(cell, str) and target.lower() in cell.lower(): # .lower so that it is case-insensitive
-                print(f"[DEBUG] Match found in cell: {cell}")
-                # If both conditions(checks above) are true, increase the counter below by 1
-                count += 1
-    print(f"'{target}' was found {count} times in the spreadsheet.")
+    target_count = {} # Counter for the target
+    
+    
+
+    for target in targets:
+        print(f"[DEBUG] Starting search or target word: {target}") # Debugging
+        count = 0
+        # Loop through each column name in the df 
+        for column in df.columns: 
+            print(f"[DEBUG] Inspecting column: {column}") # Debugging
+            # For each column, loop through every cell
+            for cell in df[column]:
+                # Make sure the cell is a string before searching
+                # Check 1. is the cell a strin (isinstance)
+                # Check 2. Does the target word appear in the cell
+                if isinstance(cell, str) and target.lower() in cell.lower(): # .lower so that it is case-insensitive
+                    print(f"[DEBUG] Match found in cell: {cell}")
+                    # If both conditions(checks above) are true, increase the counter below by 1
+                    count += 1
+        target_count[target] = count
+
+
+    for target, count in target_count.items():
+        print(f"'{target}' was found {count} times in the spreadsheet.")
 
 
 
